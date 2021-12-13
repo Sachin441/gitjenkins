72ee07ab5e466a1c752b95d5566181eb4773f957
Sachin Gupta <sachingupta2846@gmail.com>
2021-12-13 11:19:56 +0530

    changes

--- src/app/loginpage/loginpage.page.spec.ts
+++ src/app/loginpage/loginpage.page.spec.ts
@@ -1,18 +1,23 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClient} from '@angular/common/http';
 import { LoginpagePage } from './loginpage.page';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 describe('LoginpagePage', () => {
   let component: LoginpagePage;
   let fixture: ComponentFixture<LoginpagePage>;
+  let httpClient: HttpClient;
+  let httpMock: HttpTestingController;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginpagePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(),HttpClientTestingModule,ReactiveFormsModule,FormsModule,RouterTestingModule]
     }).compileComponents();
-
+    httpClient = TestBed.inject(HttpClient);
+    httpMock = TestBed.get(HttpTestingController);
     fixture = TestBed.createComponent(LoginpagePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/loginpage/loginpage.page.ts
+++ src/app/loginpage/loginpage.page.ts
@@ -13,10 +13,7 @@ export class LoginpagePage implements OnInit {
   isSubmitted = false;
 
   constructor(public formBuilder: FormBuilder)
-  {     this.ionicForm = this.formBuilder.group({
-    username: ['', [Validators.required, Validators.minLength(4),Validators.maxLength(30)]],
-    password:['', [Validators.required,Validators.minLength(5)]]
-  });
+  {
 }
 
   login() {
@@ -24,6 +21,10 @@ export class LoginpagePage implements OnInit {
   }
 
   ngOnInit() {
+    this.ionicForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4),Validators.maxLength(30)]],
+      password:['', [Validators.required,Validators.minLength(5)]]
+    });
   }
 
 }
